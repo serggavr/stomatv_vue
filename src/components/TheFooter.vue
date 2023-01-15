1461bd439b63917a63428cae56a5b3fb5390915b
GSergey <g.sergey@bk.ru>
2023-01-15 20:53:11 +0300

    add prototype

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="footer">
+    <div class="footer__wrapper">
+      <div class="footer__container">
+        <div class="footer__title-container">футтер</div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,45 @@
+<template>
+  <header class="header">
+    <div class="header__wrapper">
+      <div class="header__container">
+        <div class="header__title-container">
+          <h1 class="header__title">Воркутинская стоматологическая поликлиника</h1>
+          <p class="header__subtitle">
+            Государственное автономное учреждение здравоохранения Республики Коми
+          </p>
+        </div>
+        <img class="header__logo" src="../assets/dentist.svg" alt="Зубик" />
+        <button class="button button__placed_header">Записаться на прием</button>
+      </div>
+
+      <nav class="navigation-menu">
+        <ul class="navigation-menu__list">
+          <li class="navigation-menu__item">
+            <a href="#" class="navigation-menu__link">Главная</a>
+          </li>
+          <li class="navigation-menu__item">
+            <a href="#" class="navigation-menu__link">Новости</a>
+          </li>
+          <li class="navigation-menu__item">
+            <a href="#" class="navigation-menu__link">Об учреждении</a>
+          </li>
+          <li class="navigation-menu__item">
+            <a href="#" class="navigation-menu__link">Пациенту</a>
+          </li>
+          <li class="navigation-menu__item">
+            <a href="#" class="navigation-menu__link">Специалисту</a>
+          </li>
+          <li class="navigation-menu__item">
+            <a href="#" class="navigation-menu__link">Обратная связь</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
